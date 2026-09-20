953d8db3b51268ddbb0a09649bc276229ebf28a5
Snapegit <2017124330@qq.com>
2026-09-21 01:41:00 +0800

    Initial commit

---
+++ 3.VueUser/src/pages/jingdianxinxi/add.vue
@@ -0,0 +1,596 @@
+<template>
+<div :style='{"width":"100%","padding":"30px 7% 40px","margin":"0px auto","position":"relative","background":"#fff"}'>
+    <el-form
+	  :style='{"border":"1px solid #f0f0f0","width":"100%","padding":"30px","position":"relative","background":"#fcfcfc"}'
+      class="add-update-preview"
+      ref="ruleForm"
+      :model="ruleForm"
+      :rules="rules"
+      label-width="80px"
+    >
+          <el-form-item :style='{"padding":"10px","margin":"0 0 10px","background":"none"}' label="景点名称" prop="jingdianmingcheng">
+            <el-input v-model="ruleForm.jingdianmingcheng" 
+                placeholder="景点名称" clearable :disabled=" false  ||ro.jingdianmingcheng"></el-input>
+          </el-form-item>
+          <el-form-item :style='{"padding":"10px","margin":"0 0 10px","background":"none"}'  label="景点类型" prop="jingdianleixing">
+            <el-select v-model="ruleForm.jingdianleixing" placeholder="请选择景点类型" :disabled=" false  ||ro.jingdianleixing" >
+              <el-option
+                  v-for="(item,index) in jingdianleixingOptions"
+                  :key="index"
+                  :label="item"
+                  :value="item">
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item :style='{"padding":"10px","margin":"0 0 10px","background":"none"}' label="景点图片" v-if="type!='cross' || (type=='cross' && !ro.jingdiantupian)" prop="jingdiantupian">
+            <file-upload
+            tip="点击上传景点图片"
+            action="file/upload"
+            :limit="3"
+            :multiple="true"
+            :fileUrls="ruleForm.jingdiantupian?ruleForm.jingdiantupian:''"
+            @change="jingdiantupianUploadChange"
+            ></file-upload>
+          </el-form-item>
+            <el-form-item :style='{"padding":"10px","margin":"0 0 10px","background":"none"}' class="upload" v-else label="景点图片" prop="jingdiantupian">
+                <img v-if="ruleForm.jingdiantupian.substring(0,4)=='http'" class="upload-img" style="margin-right:20px;" v-bind:key="index" :src="ruleForm.jingdiantupian.split(',')[0]" width="100" height="100">
+                <img v-else class="upload-img" style="margin-right:20px;" v-bind:key="index" v-for="(item,index) in ruleForm.jingdiantupian.split(',')" :src="baseUrl+item" width="100" height="100">
+            </el-form-item>
+          <el-form-item :style='{"padding":"10px","margin":"0 0 10px","background":"none"}' label="门票价格" prop="menpiaojiage">
+			<el-input-number v-model="ruleForm.menpiaojiage" placeholder="门票价格" :readonly="ro.menpiaojiage"></el-input-number>
+          </el-form-item>
+          <el-form-item :style='{"padding":"10px","margin":"0 0 10px","background":"none"}' label="开放时间" prop="kaifangshijian">
+            <el-input v-model="ruleForm.kaifangshijian" 
+                placeholder="开放时间" clearable :disabled=" false  ||ro.kaifangshijian"></el-input>
+          </el-form-item>
+          <el-form-item :style='{"padding":"10px","margin":"0 0 10px","background":"none"}' label="景点发布" prop="jingdianfabu">
+              <el-date-picker
+				  :disabled=" false  ||ro.jingdianfabu"
+                  value-format="yyyy-MM-dd HH:mm:ss"
+                  v-model="ruleForm.jingdianfabu" 
+                  type="datetime"
+                  placeholder="景点发布">
+              </el-date-picker>
+          </el-form-item>
+          <el-form-item :style='{"padding":"10px","margin":"0 0 10px","background":"none"}' label="景点地址" prop="jingdiandizhi">
+            <el-input
+              type="textarea"
+              :rows="8"
+              placeholder="景点地址"
+              v-model="ruleForm.jingdiandizhi">
+            </el-input>
+          </el-form-item>
+          <el-form-item :style='{"padding":"10px","margin":"0 0 10px","background":"none"}' label="历史背景" prop="lishibeijing">
+            <el-input
+              type="textarea"
+              :rows="8"
+              placeholder="历史背景"
+              v-model="ruleForm.lishibeijing">
+            </el-input>
+          </el-form-item>
+          <el-form-item :style='{"padding":"10px","margin":"0 0 10px","background":"none"}' label="特色活动" prop="tesehuodong">
+            <el-input
+              type="textarea"
+              :rows="8"
+              placeholder="特色活动"
+              v-model="ruleForm.tesehuodong">
+            </el-input>
+          </el-form-item>
+          <el-form-item :style='{"padding":"10px","margin":"0 0 10px","background":"none"}' label="景点介绍" prop="jingdianjieshao">
+            <editor 
+                :style='{"minHeight":"250px","padding":"0","margin":"0","borderColor":"#1abc9e30","backgroundColor":"none","borderRadius":"0","borderWidth":"0px","width":"100%","borderStyle":"solid","height":"auto"}'
+                v-model="ruleForm.jingdianjieshao" 
+                class="editor" 
+                action="file/upload">
+            </editor>
+          </el-form-item>
+
+      <el-form-item :style='{"padding":"0","margin":"0"}'>
+        <el-button :style='{"border":"0","cursor":"pointer","padding":"0","margin":"0 20px 0 0","outline":"none","color":"rgba(255, 255, 255, 1)","borderRadius":"0px","background":"#1abc9e","width":"128px","lineHeight":"40px","fontSize":"14px","height":"40px"}'  type="primary" @click="onSubmit">提交</el-button>
+        <el-button :style='{"border":"1px solid #eee","cursor":"pointer","padding":"0","margin":"0","outline":"none","color":"#333","borderRadius":"0px","background":"#fcfcfc","width":"128px","lineHeight":"40px","fontSize":"14px","height":"40px"}' @click="back()">返回</el-button>
+      </el-form-item>
+    </el-form>
+</div>
+</template>
+
+<script>
+  export default {
+    data() {
+	  let self = this
+      return {
+        id: '',
+        baseUrl: '',
+        ro:{
+				jingdianmingcheng : false,
+				jingdianleixing : false,
+				jingdiantupian : false,
+				menpiaojiage : false,
+				kaifangshijian : false,
+				jingdiandizhi : false,
+				jingdianjieshao : false,
+				lishibeijing : false,
+				jingdianfabu : false,
+				tesehuodong : false,
+				thumbsupnum : false,
+				crazilynum : false,
+				clicktime : false,
+				clicknum : false,
+				discussnum : false,
+				storeupnum : false,
+        },
+        type: '',
+        userTableName: localStorage.getItem('UserTableName'),
+        ruleForm: {
+          jingdianmingcheng: '',
+          jingdianleixing: '',
+          jingdiantupian: '',
+          menpiaojiage: '',
+          kaifangshijian: '',
+          jingdiandizhi: '',
+          jingdianjieshao: '',
+          lishibeijing: '',
+          jingdianfabu: '',
+          tesehuodong: '',
+          thumbsupnum: '',
+          crazilynum: '',
+          clicktime: '',
+          clicknum: '',
+          discussnum: '',
+          storeupnum: '',
+        },
+        jingdianleixingOptions: [],
+
+
+        rules: {
+          jingdianmingcheng: [
+            { required: true, message: '景点名称不能为空', trigger: 'blur' },
+          ],
+          jingdianleixing: [
+          ],
+          jingdiantupian: [
+          ],
+          menpiaojiage: [
+            { required: true, message: '门票价格不能为空', trigger: 'blur' },
+            { validator: this.$validate.isNumber, trigger: 'blur' },
+          ],
+          kaifangshijian: [
+          ],
+          jingdiandizhi: [
+          ],
+          jingdianjieshao: [
+          ],
+          lishibeijing: [
+          ],
+          jingdianfabu: [
+          ],
+          tesehuodong: [
+          ],
+          thumbsupnum: [
+            { validator: this.$validate.isIntNumer, trigger: 'blur' },
+          ],
+          crazilynum: [
+            { validator: this.$validate.isIntNumer, trigger: 'blur' },
+          ],
+          clicktime: [
+          ],
+          clicknum: [
+            { validator: this.$validate.isIntNumer, trigger: 'blur' },
+          ],
+          discussnum: [
+            { validator: this.$validate.isIntNumer, trigger: 'blur' },
+          ],
+          storeupnum: [
+            { validator: this.$validate.isIntNumer, trigger: 'blur' },
+          ],
+        },
+		centerType: false,
+      };
+    },
+    computed: {
+
+
+
+    },
+    components: {
+    },
+    created() {
+		if(this.$route.query.centerType){
+			this.centerType = true
+		}
+	  //this.bg();
+      let type = this.$route.query.type ? this.$route.query.type : '';
+      this.init(type);
+      this.baseUrl = this.$config.baseUrl;
+      this.ruleForm.jingdianfabu = this.getCurDateTime()
+    },
+    methods: {
+      getMakeZero(s) {
+          return s < 10 ? '0' + s : s;
+      },
+      // 下载
+      download(file){
+        window.open(`${file}`)
+      },
+      // 初始化
+      init(type) {
+        this.type = type;
+        if(type=='cross'){
+          var obj = JSON.parse(localStorage.getItem('crossObj'));
+          for (var o in obj){
+            if(o=='jingdianmingcheng'){
+              this.ruleForm.jingdianmingcheng = obj[o];
+              this.ro.jingdianmingcheng = true;
+              continue;
+            }
+            if(o=='jingdianleixing'){
+              this.ruleForm.jingdianleixing = obj[o];
+              this.ro.jingdianleixing = true;
+              continue;
+            }
+            if(o=='jingdiantupian'){
+              this.ruleForm.jingdiantupian = obj[o].split(",")[0];
+              this.ro.jingdiantupian = true;
+              continue;
+            }
+            if(o=='menpiaojiage'){
+              this.ruleForm.menpiaojiage = obj[o];
+              this.ro.menpiaojiage = true;
+              continue;
+            }
+            if(o=='kaifangshijian'){
+              this.ruleForm.kaifangshijian = obj[o];
+              this.ro.kaifangshijian = true;
+              continue;
+            }
+            if(o=='jingdiandizhi'){
+              this.ruleForm.jingdiandizhi = obj[o];
+              this.ro.jingdiandizhi = true;
+              continue;
+            }
+            if(o=='jingdianjieshao'){
+              this.ruleForm.jingdianjieshao = obj[o];
+              this.ro.jingdianjieshao = true;
+              continue;
+            }
+            if(o=='lishibeijing'){
+              this.ruleForm.lishibeijing = obj[o];
+              this.ro.lishibeijing = true;
+              continue;
+            }
+            if(o=='jingdianfabu'){
+              this.ruleForm.jingdianfabu = obj[o];
+              this.ro.jingdianfabu = true;
+              continue;
+            }
+            if(o=='tesehuodong'){
+              this.ruleForm.tesehuodong = obj[o];
+              this.ro.tesehuodong = true;
+              continue;
+            }
+            if(o=='thumbsupnum'){
+              this.ruleForm.thumbsupnum = obj[o];
+              this.ro.thumbsupnum = true;
+              continue;
+            }
+            if(o=='crazilynum'){
+              this.ruleForm.crazilynum = obj[o];
+              this.ro.crazilynum = true;
+              continue;
+            }
+            if(o=='clicktime'){
+              this.ruleForm.clicktime = obj[o];
+              this.ro.clicktime = true;
+              continue;
+            }
+            if(o=='clicknum'){
+              this.ruleForm.clicknum = obj[o];
+              this.ro.clicknum = true;
+              continue;
+            }
+            if(o=='discussnum'){
+              this.ruleForm.discussnum = obj[o];
+              this.ro.discussnum = true;
+              continue;
+            }
+            if(o=='storeupnum'){
+              this.ruleForm.storeupnum = obj[o];
+              this.ro.storeupnum = true;
+              continue;
+            }
+          }
+        }else if(type=='edit'){
+			this.info()
+		}
+        // 获取用户信息
+        this.$http.get(this.userTableName + '/session', {emulateJSON: true}).then(res => {
+          if (res.data.code == 0) {
+            var json = res.data.data;
+          }
+        });
+        this.$http.get('option/jingdianleixing/jingdianleixing', {emulateJSON: true}).then(res => {
+          if (res.data.code == 0) {
+            this.jingdianleixingOptions = res.data.data;
+          }
+        });
+
+		if (localStorage.getItem('raffleType') && localStorage.getItem('raffleType') != null) {
+			localStorage.removeItem('raffleType')
+			setTimeout(() => {
+				this.onSubmit()
+			}, 300)
+		}
+      },
+
+    // 多级联动参数
+      // 多级联动参数
+      info() {
+        this.$http.get(`jingdianxinxi/detail/${this.$route.query.id}`, {emulateJSON: true}).then(res => {
+          if (res.data.code == 0) {
+            this.ruleForm = res.data.data;
+          }
+        });
+      },
+      // 提交
+      onSubmit() {
+
+			//更新跨表属性
+			var crossuserid;
+			var crossrefid;
+			var crossoptnum;
+			this.$refs["ruleForm"].validate(valid => {
+				if(valid) {
+					if(this.type=='cross'){
+						var statusColumnName = localStorage.getItem('statusColumnName');
+						var statusColumnValue = localStorage.getItem('statusColumnValue');
+						if(statusColumnName && statusColumnName!='') {
+							var obj = JSON.parse(localStorage.getItem('crossObj'));
+							if(!statusColumnName.startsWith("[")) {
+								for (var o in obj){
+									if(o==statusColumnName){
+										obj[o] = statusColumnValue;
+									}
+								}
+								var table = localStorage.getItem('crossTable');
+								this.$http.post(table+'/update', obj).then(res => {});
+							} else {
+								crossuserid=Number(localStorage.getItem('frontUserid'));
+								crossrefid=obj['id'];
+								crossoptnum=localStorage.getItem('statusColumnName');
+								crossoptnum=crossoptnum.replace(/\[/,"").replace(/\]/,"");
+							}
+						}
+					}
+					if(crossrefid && crossuserid) {
+						this.ruleForm.crossuserid=crossuserid;
+						this.ruleForm.crossrefid=crossrefid;
+						var params = {
+							page: 1,
+							limit: 10,
+							crossuserid:crossuserid,
+							crossrefid:crossrefid,
+						}
+						this.$http.get('jingdianxinxi/list', {
+							params: params
+						}).then(res => {
+							if(res.data.data.total>=crossoptnum) {
+								this.$message({
+									message: localStorage.getItem('tips'),
+									type: 'error',
+									duration: 1500,
+								});
+								return false;
+							} else {
+								// 跨表计算
+
+
+								this.$http.post(`jingdianxinxi/${this.ruleForm.id?'update':this.centerType?'save':'add'}`, this.ruleForm).then(res => {
+									if (res.data.code == 0) {
+										this.$message({
+											message: '操作成功',
+											type: 'success',
+											duration: 1500,
+											onClose: () => {
+												this.$router.go(-1);
+											}
+										});
+									} else {
+										this.$message({
+											message: res.data.msg,
+											type: 'error',
+											duration: 1500
+										});
+									}
+								});
+							}
+						});
+					} else {
+
+
+						this.$http.post(`jingdianxinxi/${this.ruleForm.id?'update':this.centerType?'save':'add'}`, this.ruleForm).then(res => {
+							if (res.data.code == 0) {
+								this.$message({
+									message: '操作成功',
+									type: 'success',
+									duration: 1500,
+									onClose: () => {
+										this.$router.go(-1);
+									}
+								});
+							} else {
+								this.$message({
+									message: res.data.msg,
+									type: 'error',
+									duration: 1500
+								});
+							}
+						});
+					}
+				}
+			});
+		},
+		// 获取uuid
+		getUUID () {
+			return new Date().getTime();
+		},
+		// 返回
+		back() {
+			this.$router.go(-1);
+		},
+      jingdiantupianUploadChange(fileUrls) {
+          this.ruleForm.jingdiantupian = fileUrls.replace(new RegExp(this.$config.baseUrl,"g"),"");;
+      },
+    }
+  };
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+	.el-date-editor.el-input {
+		width: auto;
+	}
+	
+	.add-update-preview .el-form-item ::v-deep .el-form-item__label {
+	  padding: 0 10px 0 0;
+	  color: #666;
+	  font-weight: 500;
+	  width: 80px;
+	  font-size: 14px;
+	  line-height: 40px;
+	  text-align: right;
+	}
+	
+	.add-update-preview .el-form-item ::v-deep .el-form-item__content {
+	  margin-left: 80px;
+	}
+	
+	.add-update-preview .el-input ::v-deep .el-input__inner {
+	  border: 1px solid #ddd;
+	  padding: 0 12px;
+	  color: #666;
+	  font-size: 14px;
+	  border-color: #1abc9e30;
+	  border-radius: 0px;
+	  box-shadow: 0 0 0px rgba(64, 158, 255, .5);
+	  outline: none;
+	  background: none;
+	  width: 400px;
+	  border-width: 0 0 1px;
+	  border-style: solid;
+	  height: 40px;
+	}
+	.add-update-preview .el-input-number ::v-deep .el-input__inner {
+		text-align: left;
+	  border: 1px solid #ddd;
+	  padding: 0 12px;
+	  color: #666;
+	  font-size: 14px;
+	  border-color: #1abc9e30;
+	  border-radius: 0px;
+	  box-shadow: 0 0 0px rgba(64, 158, 255, .5);
+	  outline: none;
+	  background: none;
+	  width: 400px;
+	  border-width: 0 0 1px;
+	  border-style: solid;
+	  height: 40px;
+	}
+	.add-update-preview .el-input-number ::v-deep .el-input-number__decrease {
+		display: none;
+	}
+	.add-update-preview .el-input-number ::v-deep .el-input-number__increase {
+		display: none;
+	}
+	
+	.add-update-preview .el-select ::v-deep .el-input__inner {
+	  border-radius: 0px;
+	  padding: 0 10px;
+	  color: #666;
+	  background: none;
+	  width: 200px;
+	  font-size: 14px;
+	  border-color: #1abc9e30;
+	  border-width: 0 0 1px;
+	  border-style: solid;
+	  height: 40px;
+	}
+	
+	.add-update-preview .el-date-editor ::v-deep .el-input__inner {
+	  border-radius: 0px;
+	  padding: 0 10px 0 30px;
+	  color: #666;
+	  background: none;
+	  width: 200px;
+	  font-size: 14px;
+	  border-color: #1abc9e30;
+	  border-width: 0 0 1px;
+	  border-style: solid;
+	  height: 40px;
+	}
+	
+	.add-update-preview ::v-deep .el-upload--picture-card {
+		background: transparent;
+		border: 0;
+		border-radius: 0;
+		width: auto;
+		height: auto;
+		line-height: initial;
+		vertical-align: middle;
+	}
+	
+	.add-update-preview ::v-deep .upload .upload-img {
+	  cursor: pointer;
+	  border-radius: 0px;
+	  color: #1abc9e60;
+	  background: none;
+	  width: 200px;
+	  font-size: 32px;
+	  border-color: #1abc9e30;
+	  border-width: 0 0 1px;
+	  line-height: 100px;
+	  border-style: solid;
+	  text-align: center;
+	  height: 100px;
+	}
+	
+	.add-update-preview ::v-deep .el-upload-list .el-upload-list__item {
+	  cursor: pointer;
+	  border-radius: 0px;
+	  color: #1abc9e60;
+	  background: none;
+	  width: 200px;
+	  font-size: 32px;
+	  border-color: #1abc9e30;
+	  border-width: 0 0 1px;
+	  line-height: 100px;
+	  border-style: solid;
+	  text-align: center;
+	  height: 100px;
+	}
+	
+	.add-update-preview ::v-deep .el-upload .el-icon-plus {
+	  cursor: pointer;
+	  border-radius: 0px;
+	  color: #1abc9e60;
+	  background: none;
+	  width: 200px;
+	  font-size: 32px;
+	  border-color: #1abc9e30;
+	  border-width: 0 0 1px;
+	  line-height: 100px;
+	  border-style: solid;
+	  text-align: center;
+	  height: 100px;
+	}
+	
+	.add-update-preview .el-textarea ::v-deep .el-textarea__inner {
+	  border: 1px solid #1abc9e30;
+	  border-radius: 0px;
+	  padding: 12px;
+	  box-shadow: 0 0 0px rgba(64, 158, 255, .5);
+	  outline: none;
+	  color: #666;
+	  background: none;
+	  width: 400px;
+	  font-size: 14px;
+	  height: 120px;
+	}
+</style>
